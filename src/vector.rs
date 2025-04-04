1ca5048b27bdc428e8cdbd813156b4ffd3374340
Kevin Ma <kevin_ma@ycsoftwares.com>
2025-04-04 22:53:06 +0800

    perf: 改进矩阵乘法算法, 使用并发处理矩阵乘法

--- src/vector.rs
+++ src/vector.rs
@@ -1,4 +1,4 @@
-use std::ops::Deref;
+use std::ops::{Add, AddAssign, Deref, Mul};
 
 pub struct Vector<T> {
     data: Vec<T>,
@@ -16,4 +16,18 @@ impl<T> Deref for Vector<T> {
     fn deref(&self) -> &Self::Target {
         &self.data
     }
+}
+
+pub fn dot_product<T>(a: Vector<T>, b: Vector<T>) -> anyhow::Result<T>
+where
+    T: Copy + Default + AddAssign + Add<Output = T> + Mul<Output = T>,
+{
+    if a.len() != b.len() {
+        return Err(anyhow::anyhow!("点乘错误: a.len() != b.len()"));
+    }
+    let mut sum = T::default();
+    for i in 0..a.len() {
+        sum += a[i] * b[i];
+    }
+    Ok(sum)
 }
